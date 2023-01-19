961f23d7134fe2d52eb01457e59ff2811a0c6a7a
kubat <kubat.aiupov@gmail.com>
2023-01-19 21:46:21 +0100

    init mex-juices, still in progress. init timeToMixJuice func

---
+++ mixed-juices.js
@@ -0,0 +1,27 @@
+function timeToMixJuice(name) {
+  switch (name) {
+    case "Pure Strawberry Joy":
+      return 0.5;
+      break;
+    case "Energizer":
+      return 1.5;
+      break;
+
+    case "Green Garden":
+      return 1.5;
+
+    case "Tropical Island":
+      return 3;
+      break;
+
+    case "All or Nothing":
+      return 5;
+      break;
+    default:
+      return 2.5;
+  }
+}
+
+// still in progress
+
+
